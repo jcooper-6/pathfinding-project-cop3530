75ba329d3c9d53a5d5182f6d4537313d263ac645
John <jcooper6@ufl.edu>
2025-12-02 17:02:19 -0500

    Added reset_map, brush terrain drawing and erasing feature and brush selection buttons

--- pathfinding-project-cop3530/pathfinding-project-cop3530.cpp
+++ pathfinding-project-cop3530/pathfinding-project-cop3530.cpp
@@ -5,44 +5,115 @@
 #include <vector>
 #include <iostream>
 
+// Constants
+const int CELL_SIZE = 2;
+const int COLS = 630;
+const int ROWS = 310;
+const int BRUSH_RADIUS = 2;
+
 struct Cell {
     bool is_wall = false;
     bool visited = false;
-    float cost = 1.f;          
-    int cell_size = 2;
+    float cost = 1.f;
 };
 
 void test_func() {
     std::cout << "Do Something" << std::endl;
 }
 
+void reset_map(std::vector<std::vector<Cell>>& map, sf::VertexArray& map_vertex_array) {
+    for (int r = 0; r < ROWS; r++) {
+        for (int c = 0; c < COLS; c++) {
+            map[r][c].cost = 1.f;
+
+            int index = (r * COLS + c) * 4;
+            for (int i = 0; i < 4; i++) {
+                sf::Color grey(128, 128, 128);
+                map_vertex_array[index + i].color = grey;
+            }
+
+        }
+    }
+}
+
+void brush(std::vector<std::vector<Cell>>& map, sf::VertexArray& map_vertex_array, std::string& brush_type, sf::Vector2i mouse_pos) {
+
+
+    int col = (mouse_pos.x - 10) / CELL_SIZE;
+    int row = (mouse_pos.y - 10) / CELL_SIZE;
+    if (col < 0 || col >= COLS || row < 0 || row >= ROWS)
+        return;
+
+    float cost = 1.f;
+    int b_radius = BRUSH_RADIUS;
+    sf::Color color = sf::Color(128, 128, 128);
+
+    if (brush_type == "forest") {
+        color = sf::Color::Green;
+        cost = 50.f;
+    }
+    else if (brush_type == "river") {
+        color = sf::Color::Blue;
+        cost = 200.f;
+    }
+    else if (brush_type == "mountain") {
+        color = sf::Color::Black;
+        cost = 400.f;
+    }
+    else if (brush_type == "erase") {
+        color = sf::Color(128, 128, 128);
+        cost = 1.f;
+        b_radius = 5;
+    }
+
+
+    for (int dr = -b_radius; dr <= b_radius; dr++) {
+        for (int dc = -b_radius; dc <= b_radius; dc++) {
+
+            int rr = row + dr;
+            int cc = col + dc;
+
+            // Boundary check
+            if (rr < 0 || rr >= ROWS || cc < 0 || cc >= COLS)
+                continue;
+
+            int index = (rr * COLS + cc) * 4;
+            // Update terrain cost in map
+            map[rr][cc].cost = cost;
+
+            // Update vertex array color
+            for (int i = 0; i < 4; i++) {
+                map_vertex_array[index + i].color = color;
+            }
+
+        }
+    }
+}
+
 int main()
 {
     // Initialize Grid/Map Information
-    int cols = 630; // 1260/2
-    int rows = 310; // 620/2
     // Total cells/nodes in graph/grid =  195,300
     // Each cell is 2x2 pixels, and the whole map covers a 1260x620 space on the screen
-    std::vector<std::vector<Cell>> map(rows, std::vector<Cell>(cols)); // Data for pathfinding
+    std::vector<std::vector<Cell>> map(ROWS, std::vector<Cell>(COLS)); // Data for pathfinding
     // SFML VertexArray to draw the entire grid with one draw() call for efficiency.
-    sf::VertexArray map_vertex_array(sf::Quads, rows * cols * 4);
+    sf::VertexArray map_vertex_array(sf::Quads, ROWS * COLS * 4);
 
     // Initialize vertices in the 1D vertexarray, 4 vertices = 1 Quad/Cell
-    for (int r = 0; r < rows; r++) {
-        for (int c = 0; c < cols; c++) {
+    for (int r = 0; r < ROWS; r++) {
+        for (int c = 0; c < COLS; c++) {
             
             // Index is the current cell we want to create vertices for
-            int index = (r * cols + c) * 4; 
-            int cell_size = map[r][c].cell_size;
+            int index = (r * COLS + c) * 4;
             // + 10 creates a nice border so the map sits in the middle of the screen.
             // Top-left
-            map_vertex_array[index + 0].position = sf::Vector2f(c * cell_size + 10, r * cell_size + 10);
+            map_vertex_array[index + 0].position = sf::Vector2f(c * CELL_SIZE + 10, r * CELL_SIZE + 10);
             // Top-right
-            map_vertex_array[index + 1].position = sf::Vector2f((c + 1) * cell_size + 10, r * cell_size + 10);
+            map_vertex_array[index + 1].position = sf::Vector2f((c + 1) * CELL_SIZE + 10, r * CELL_SIZE + 10);
             // Bottom-right
-            map_vertex_array[index + 2].position = sf::Vector2f((c + 1) * cell_size + 10, (r + 1) * cell_size + 10);
+            map_vertex_array[index + 2].position = sf::Vector2f((c + 1) * CELL_SIZE + 10, (r + 1) * CELL_SIZE + 10);
             // Bottom-left
-            map_vertex_array[index + 3].position = sf::Vector2f(c * cell_size + 10, (r + 1) * cell_size + 10);
+            map_vertex_array[index + 3].position = sf::Vector2f(c * CELL_SIZE + 10, (r + 1) * CELL_SIZE + 10);
 
             // Default color
             for (int i = 0; i < 4; i++) {
@@ -67,17 +138,24 @@ int main()
     // Create window
     sf::RenderWindow window(sf::VideoMode(1280, 720), "pathfinding-sfml-project");
 
+    // Brush Functions and Variables
+    std::string current_brush = "forest";
+
     // Create buttons
     buttons.push_back(Button(sf::Vector2f(100, 60), sf::Vector2f(535, 650), "Start", font, test_func));
-    buttons.push_back(Button(sf::Vector2f(100, 60), sf::Vector2f(645, 650), "Reset", font, test_func));
+    buttons.push_back(Button(sf::Vector2f(100, 60), sf::Vector2f(645, 650), "Reset", font, [&]() {reset_map(map, map_vertex_array);}));
     buttons.push_back(Button(sf::Vector2f(400, 60), sf::Vector2f(10, 650), "Results Section", font, test_func));
     buttons.push_back(Button(sf::Vector2f(200, 30), sf::Vector2f(860, 640), "Dijkstra's", font, test_func));
     buttons.push_back(Button(sf::Vector2f(200, 30), sf::Vector2f(1070, 640), "A* Search", font, test_func));
-    buttons.push_back(Button(sf::Vector2f(410, 30), sf::Vector2f(860, 680), "Generate New Map", font, test_func));
+    buttons.push_back(Button(sf::Vector2f(136, 30), sf::Vector2f(860, 680), "Forest", font, [&]() {current_brush = "forest"; }));
+    buttons.push_back(Button(sf::Vector2f(136, 30), sf::Vector2f(996, 680), "River", font, [&]() {current_brush = "river"; }));
+    buttons.push_back(Button(sf::Vector2f(136, 30), sf::Vector2f(1132, 680), "Mountain", font, [&]() {current_brush = "mountain"; }));
 
+   
 
     while (window.isOpen())
     {
+        mouse_pos = sf::Mouse::getPosition(window);
         // Manage window events
         sf::Event event;
         while (window.pollEvent(event))
@@ -88,8 +166,6 @@ int main()
             // Check for mouse click
             if (event.type == sf::Event::MouseButtonPressed) {
                 if (event.mouseButton.button == sf::Mouse::Left) {
-                    // Get mouse position
-                    mouse_pos = sf::Mouse::getPosition(window);
                     // Check if mouse clicked a button
                     for (auto& button : buttons) {
                         button.isClicked(mouse_pos);
@@ -97,6 +173,18 @@ int main()
                 }
             }
         }
+        // Check for drawing and erasing
+        bool mouse_leftclick_hold = sf::Mouse::isButtonPressed(sf::Mouse::Left);
+        if (mouse_leftclick_hold) {
+            brush(map, map_vertex_array, current_brush, mouse_pos);
+        }
+        bool mouse_rightclick_hold = sf::Mouse::isButtonPressed(sf::Mouse::Right);
+        if (mouse_rightclick_hold) {
+            std::string erase = "erase";
+            brush(map, map_vertex_array, erase, mouse_pos);
+        }
+
+
         // Clear the screen, draw buttons and map, then update the display.
         window.clear(sf::Color::White);
         for (auto& button : buttons)
