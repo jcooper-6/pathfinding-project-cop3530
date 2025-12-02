d826abcfe00b34e2ffc3e1ffe5ceecdbb8840c2b
John <jcooper6@ufl.edu>
2025-12-02 15:23:43 -0500

    Created GUI for map and 2D Vertex representation of map using Cell struct

--- pathfinding-project-cop3530/pathfinding-project-cop3530.cpp
+++ pathfinding-project-cop3530/pathfinding-project-cop3530.cpp
@@ -2,7 +2,15 @@
 #include "button.h"
 #include <unordered_map>
 #include <string>
+#include <vector>
 #include <iostream>
+
+struct Cell {
+    bool is_wall = false;
+    bool visited = false;
+    float cost = 1.f;          
+    int cell_size = 2;
+};
 
 void test_func() {
     std::cout << "Do Something" << std::endl;
@@ -10,6 +18,41 @@ void test_func() {
 
 int main()
 {
+    // Initialize Grid/Map Information
+    int cols = 630; // 1260/2
+    int rows = 310; // 620/2
+    // Total cells/nodes in graph/grid =  195,300
+    // Each cell is 2x2 pixels, and the whole map covers a 1260x620 space on the screen
+    std::vector<std::vector<Cell>> map(rows, std::vector<Cell>(cols)); // Data for pathfinding
+    // SFML VertexArray to draw the entire grid with one draw() call for efficiency.
+    sf::VertexArray map_vertex_array(sf::Quads, rows * cols * 4);
+
+    // Initialize vertices in the 1D vertexarray, 4 vertices = 1 Quad/Cell
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) {
+            
+            // Index is the current cell we want to create vertices for
+            int index = (r * cols + c) * 4; 
+            int cell_size = map[r][c].cell_size;
+            // + 10 creates a nice border so the map sits in the middle of the screen.
+            // Top-left
+            map_vertex_array[index + 0].position = sf::Vector2f(c * cell_size + 10, r * cell_size + 10);
+            // Top-right
+            map_vertex_array[index + 1].position = sf::Vector2f((c + 1) * cell_size + 10, r * cell_size + 10);
+            // Bottom-right
+            map_vertex_array[index + 2].position = sf::Vector2f((c + 1) * cell_size + 10, (r + 1) * cell_size + 10);
+            // Bottom-left
+            map_vertex_array[index + 3].position = sf::Vector2f(c * cell_size + 10, (r + 1) * cell_size + 10);
+
+            // Default color
+            for (int i = 0; i < 4; i++) {
+                sf::Color grey(128, 128, 128);
+                map_vertex_array[index + i].color = grey;
+            }
+        }
+    }
+
+
     // Initialize Button Vector
     std::vector<Button> buttons;
 
@@ -54,10 +97,11 @@ int main()
                 }
             }
         }
-        // Clear the screen and draw buttons, then update the display.
+        // Clear the screen, draw buttons and map, then update the display.
         window.clear(sf::Color::White);
         for (auto& button : buttons)
             button.draw(window);
+        window.draw(map_vertex_array);
         window.display();
     }
 
